761131618951bf01168873f04aba3d4f0eb59f35
Mahjabin Alam <natasha.mahjabin@gmail.com>
2020-10-11 14:39:03 +0900

    initial commit

---
+++ MACustomAlertPopOverCombo/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MACustomAlertPopOverCombo
+//
+//  Created by Mahjabin Alam on 2020/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MACustomAlertPopOverCombo/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MACustomAlertPopOverCombo
+//
+//  Created by Mahjabin Alam on 2020/10/11.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
